14f981c0b380b3caf9d3c7df90bb4a8c4b0c5871
zayroninfotech <zayroninfotech@gmail.com>
2026-08-03 12:51:23 +0530

    new

--- ZayroDocs/urls.py
+++ ZayroDocs/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 import mimetypes
@@ -19,6 +20,8 @@ def _serve_output(request, path):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('apps.dashboard.urls')),
     path('tools/', include('apps.pdf_tools.urls')),
     path('media/outputs/<path:path>', _serve_output),
